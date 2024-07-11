b6af193b2feb9b3157a373a4edd8077451866346
Imo Madjid <mhmmadjid@gmail.com>
2024-07-12 02:05:40 +0700

    feat: auth

--- src/utils/supabase/middleware.ts
+++ src/utils/supabase/middleware.ts
@@ -40,6 +40,8 @@ export async function updateSession(request: NextRequest) {
     data: { user },
   } = await supabase.auth.getUser();
 
+  console.log("User", user);
+
   if (
     !user &&
     !request.nextUrl.pathname.startsWith("/login") &&
